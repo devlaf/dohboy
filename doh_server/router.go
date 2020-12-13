e3fba02e9b29a319dee7a7e84e23f90b483010d8
Devin LaFrance <devin@devlaf.io>
2020-12-12 19:12:55 -0600

    update router template

--- doh_server/router.go
+++ doh_server/router.go
@@ -2,14 +2,26 @@ package doh
 
 import (
 	"fmt"
+	"html"
 	"net/http"
 )
 
 func createRouter() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "Hello")
+	mux.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
+		if request.URL.Path != "/dns-query" {
+			http.NotFound(response, request)
+			return
+		}
+
+		if request.Method == "GET" {
+			fmt.Fprintf(response, "GET, %q", html.EscapeString(request.URL.Path))
+		} else if request.Method == "POST" {
+			fmt.Fprintf(response, "POST, %q", html.EscapeString(request.URL.Path))
+		} else {
+			http.Error(response, "Invalid request.", 405)
+		}
 	})
 
 	return mux
